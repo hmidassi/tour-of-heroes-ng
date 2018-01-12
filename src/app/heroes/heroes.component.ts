4bbb41a71d63aff67673e1a1a40556a04ccc82cc
hmidassi <hedy.midassi@gmail.com>
2018-01-12 16:49:02 +0100

    ajout classe Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Hero} from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero='Batman';
+  hero:Hero={
+    id:1,
+    name:'Batman',
+    trueName:'Bruce Wayne'
+  };
   constructor() { }
 
   ngOnInit() {
