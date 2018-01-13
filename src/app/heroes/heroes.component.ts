aeef24224a71d3a05b5c3e2351b250315a98f44d
hmidassi <hedy.midassi@gmail.com>
2018-01-13 23:19:57 +0100

    séparation sélection et détail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,14 +8,15 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero:Hero={
-    id:1,
-    name:'Batman',
-    trueName:'Bruce Wayne'
-  };
+  selectedHero:Hero;
+  heroes=HEROES;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero:Hero): void{
+    this.selectedHero=hero;
   }
 
 }
