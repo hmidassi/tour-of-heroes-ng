e7c827c05d546557c1121c25d269c9285c598351
hmidassi <hedy.midassi@gmail.com>
2018-01-15 16:06:44 +0100

    Routing finalisé

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,4 +15,9 @@ export class HeroService {
 
   };
 
+  getHero(id: number): Observable<Hero>{
+    this.messageService.add(`Héros récupéré id:${id}`);
+    return of(HEROES.find(hero=>hero.id===id));
+  }
+
 }
